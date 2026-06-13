0fab815cb4eae8dfac66812939e5b65bd4b899b1
Красимир Димитров <lukezester@gmail.com>
2026-06-13 09:02:01 +0300

    Fix export statement for trust_engine

--- lib/db/src/schema/index.ts
+++ lib/db/src/schema/index.ts
@@ -2,7 +2,7 @@ export * from "./projects";
 export * from "./clients";
 export * from "./contracts";
 export * from "./proposals";
-export * from "./trust_engine";
+export * from "./trust_engine"
 export * from "./users";
 export * from "./audits";
 export * from "./saga";
