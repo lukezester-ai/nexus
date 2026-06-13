41e4dfed2be2d141bbb1973255a262936e36796f
Красимир Димитров <lukezester@gmail.com>
2026-06-13 09:05:41 +0300

    Remove users export from schema index
    Removed export for users from the schema index.

--- lib/db/src/schema/index.ts
+++ lib/db/src/schema/index.ts
@@ -3,6 +3,5 @@ export * from "./clients";
 export * from "./contracts";
 export * from "./proposals";
 export * from "./trust_engine"
-export * from "./users";
 export * from "./audits";
 export * from "./saga";
